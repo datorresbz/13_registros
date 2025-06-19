fc2995f439aacd0f505318ce1f05118a4389d6d7
datorresbz <datorresb@unjbg.edu.pe>
2025-06-18 21:47:35 -0500

    Parte 4: Se mejoro el codigo y se implemetno una lista de los empleados que en el mes de diciembre tuvieron ventas inferiores a las 30 unidades

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -17,13 +17,16 @@ int main(){
         cout<<"Ingrese el nombre del empleado: ";
         cin.ignore();
         getline(cin,EMPLEADOS[i].nom);
-        cout<<"Ingrese las ventas del empleado: ";
+        cout<<"Ingrese las ventas del empleado: "<<endl;
         for(int j=0; j<12; j++){
+            cout<<"Ingrese las ventas del mes "<<j+1<<": ";
             cin>>EMPLEADOS[i].ven[j];
         }
         cout<<"Ingrese el salario del empleado: ";
         cin>>EMPLEADOS[i].sal;
+        cout<<endl;
     }
+    cout<<endl;
     for(int i=0; i<n; i++){
         cout<<"Numero del empleado: "<<EMPLEADOS[i].num<<endl;
         cout<<"Nombre del empleado: "<<EMPLEADOS[i].nom<<endl;
@@ -47,23 +50,31 @@ int main(){
             indicador=i;
         }
     }
-    cout<<"Empleado con la mayor venta:"<<endl;
+    cout<<"Empleado con la mayor venta anual:"<<endl;
     cout<<"Numero: "<<EMPLEADOS[indicador].num<<endl;
-    cout<<"Nombre: "<<EMPLEADOS[indicador].nom<<endl;
+    cout<<"Nombre: "<<EMPLEADOS[indicador].nom<<endl<<endl;
     //Parte 3//
     int ventastotales, indice;
-        for(int i=0; i<n; i++){
-            ventastotales=0;
-            for(int j=0; j<12; j++){
-                ventastotales=ventastotales+EMPLEADOS[i].ven[j];
-           }
-           if(ventastotales>100){
-                EMPLEADOS[i].sal=EMPLEADOS[i].sal*11/10;
-                cout<<"Empleado que recibio aumento:"<<endl;
-                cout<<"Numero: "<<EMPLEADOS[i].num<<endl;
-                cout<<"Nombre: "<<EMPLEADOS[i].nom<<endl;
-                cout<<"Nuevo salario: "<<EMPLEADOS[i].sal<<endl<<endl;
-           }
+    for(int i=0; i<n; i++){
+        ventastotales=0;
+        for(int j=0; j<12; j++){
+            ventastotales=ventastotales+EMPLEADOS[i].ven[j];
         }
+        if(ventastotales>100){
+            EMPLEADOS[i].sal=EMPLEADOS[i].sal*11/10;
+            cout<<"Empleado que recibio aumento:"<<endl;
+            cout<<"Numero: "<<EMPLEADOS[i].num<<endl;
+            cout<<"Nombre: "<<EMPLEADOS[i].nom<<endl;
+            cout<<"Nuevo salario: "<<EMPLEADOS[i].sal<<endl<<endl;
+       }
+    }
+    //Parte 4//
+    for(int i=0; i<n; i++){
+        if(EMPLEADOS[i].ven[11]<30){
+            cout<<"Empleado con menos de 30 ventas en diciembre(mes 12): "<<endl;
+            cout<<"Numero: "<<EMPLEADOS[i].num<<endl;
+            cout<<"Nombre: "<<EMPLEADOS[i].nom<<endl<<endl;
+        }
+    }
     return 0;
 }
