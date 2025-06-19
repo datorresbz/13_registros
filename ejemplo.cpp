0d135775ec71dd5193863102a71cd3f9ac25ab29
datorresbz <datorresb@unjbg.edu.pe>
2025-06-18 21:36:22 -0500

    Mejora en el entendimiento del usuario

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -1,35 +1,37 @@
 #include<iostream>
 #include<string>
 using namespace std;
-
 struct EMP{
     int num;
     string nom;
-    float ven[12],sal;
-
+    float ven[12], sal;
 };
 int main(){
     EMP EMPLEADOS[100];
     int n;
+    cout<<"Ingrese la cantidad de empleados: ";
     cin>>n;
-    for(int i=0;i<n;i++){
+    for(int i=0; i<n; i++){
+        cout<<"Ingrese el numero del empleado: ";
         cin>>EMPLEADOS[i].num;
+        cout<<"Ingrese el nombre del empleado: ";
         cin.ignore();
         getline(cin,EMPLEADOS[i].nom);
-        for(int j=0;j<12;j++){
+        cout<<"Ingrese las ventas del empleado: ";
+        for(int j=0; j<12; j++){
             cin>>EMPLEADOS[i].ven[j];
         }
+        cout<<"Ingrese el salario del empleado: ";
         cin>>EMPLEADOS[i].sal;
     }
-    for(int i=0;i<n;i++){
+    for(int i=0; i<n; i++){
         cout<<EMPLEADOS[i].num<<endl;
         cout<<EMPLEADOS[i].nom<<endl;
-        for(int j=0;j<12;j++){
+        for(int j=0; j<12; j++){
             cout<<EMPLEADOS[i].ven[j]<<" ";
         }
-        cout<<"\n";
+        cout<<endl;
         cout<<EMPLEADOS[i].sal<<endl<<endl;
     }
     return 0;
-    
 }
